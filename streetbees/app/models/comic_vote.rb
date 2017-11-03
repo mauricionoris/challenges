48521b278432ea93ddb4c955a35a19d5226d4e7d
Mauricio Noris <mauricionoris@gmail.com>
2017-11-03 11:45:04 -0200

    Add files via upload

---
+++ streetbees/app/models/comic_vote.rb
@@ -0,0 +1,8 @@
+class ComicVote < ApplicationRecord
+  def self.upvote(comic_id:)
+    find_or_initialize_by(comic_id: comic_id).tap do |votes|
+      votes.votes += 1
+      votes.save!
+    end
+  end
+end
